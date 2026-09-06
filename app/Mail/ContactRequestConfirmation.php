9ee3d3a83877a5aa862d28a2c1db690c8849ce1f
David Alan Garcia <david.garcia.nete@gmail.com>
2026-09-05 20:44:16 -0400

    Send requester submission confirmations

---
+++ app/Mail/ContactRequestConfirmation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\ContactRequest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactRequestConfirmation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(public ContactRequest $contactRequest) {}
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(subject: 'We received your message');
+    }
+
+    public function content(): Content
+    {
+        return new Content(view: 'mail.contact-request-confirmation');
+    }
+}
